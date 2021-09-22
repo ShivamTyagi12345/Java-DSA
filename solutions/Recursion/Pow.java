e1161ab9d4c0050b0c0bb086a928b519ce3d7066
shivam tyagi <shivamt.ug19.ec@nitp.ac.in>
2021-09-23 03:02:58 +0530

    Create Pow.java

---
+++ solutions/Recursion/Pow.java
@@ -0,0 +1,26 @@
+ public double myPow(double x, int n) {
+           if(n==0)
+        return 1;
+    //if n is minimum--kinda imp
+    if(n==Integer.MIN_VALUE)
+    {
+        return myPow(x*x,n/2);
+    }
+    //if n is negative
+    if(n<0)
+    {
+        x=1/x;
+        n=-n;
+    }
+    //if n is even
+    if(n%2==0)
+    {
+        return myPow(x*x,n/2);
+    }
+    //if n is odd
+    else
+    {
+        return x*myPow(x*x,n/2);
+    }
+
+    }
